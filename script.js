8eb9612070686999167688434322ed1c43089f69
amin-dev-tech <amin.dev.tech@gmail.com>
2024-06-29 15:35:57 +0300

    add loader page

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// document
-//   .getElementById("tap-effect")
-//   .addEventListener("click", function (event) {
-//     const img = this;
-//     const rect = img.getBoundingClientRect();
-//     const imgX = rect.left + rect.width / 2;
-//     const imgY = rect.top + rect.height / 2;
-
-//     const clickX = event.clientX;
-//     const clickY = event.clientY;
-
-//     const threshold = rect.width * 0.1;
-//     const distanceX = Math.abs(clickX - imgX);
-//     const distanceY = Math.abs(clickY - imgY);
-
-//     if (distanceX < threshold && distanceY < threshold) {
-//       img.style.transform = "scale(0.95)";
-//     } else {
-//       const rotateY = ((clickX - imgX) / rect.width) * 80;
-//       const rotateX = ((imgY - clickY) / rect.height) * 80;
-
-//       img.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.05)`;
-//     }
-
-//     setTimeout(() => {
-//       img.style.transform = "rotateY(0deg) rotateX(0deg) scale(1)";
-//     }, 130);
-//   });
-
 document
   .getElementById("tap-effect")
   .addEventListener("click", function (event) {
@@ -48,10 +19,9 @@ document
       const rotateY = ((clickX - imgX) / rect.width) * 30;
       const rotateX = ((imgY - clickY) / rect.height) * 30;
 
-      img.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.05)`;
+      img.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.2)`;
     }
 
-    // Create and animate +1 element
     const plusOne = document.createElement("div");
     plusOne.className = "plus-one";
     plusOne.innerText = "+1";
